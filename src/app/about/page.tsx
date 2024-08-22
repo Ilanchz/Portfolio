bce65a7a0044c066c52e8f19b702c10c9e936d1d
Ilan <vilanchezhiyan@gmail.com>
2024-08-22 22:59:22 +0530

    Final commit

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-      <section>
-        <h2 className="text-3xl font-bold">About Me</h2>
-        <p className="mt-4">Hello! I'm a developer with experience in React, Next.js, Angular, Python, Flask, and more.</p>
-      </section>
-    );
-  }
-  
